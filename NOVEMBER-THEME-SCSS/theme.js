9f3296c0a7a9ec1eab997f99740678e6e843d364
Joe Watson SBF <joewatson40@hotmail.com>
2022-10-27 19:39:29 -0500

    new style added

--- NOVEMBER-THEME-SCSS/theme.js
+++ NOVEMBER-THEME-SCSS/theme.js
@@ -1,43 +1,55 @@
 
 
-// GO in theme.js in line 3506 remove the slice property
+// GO in theme.js change line [3498 to 3545] to this:
 
-var desc = product.description;
-
-
-
-
-// Go ahead now in lien 3533 make this change
-
-//BEFORE CHANGE 
-
-var imageCount = $(images).length;
-$(images).each(function (i, image) {
-    image_embed = '<div><img src="' + image + '"></div>';
-    image_embed = image_embed.replace('.jpg', '_800x.jpg').replace('.png', '_800x.png');
-    $(quickviewThumb).append(image_embed);
-});
-$(quickviewThumb).slick({
-    rtl: theme.rtl,
-    dots: true,
-    arrows: true,
-    respondTo: 'min',
-    useTransform: true
-}).css('opacity', '1');
-
-
-//AFTER CHANGE
 
 
 // COLLABO CUSTOM JS
 
-var imageCount = $(images).length;
+    
+    //Pushdata
+    $(quickviewId).addClass(product_handle).data('handle',product_handle);
+    jQuery.getJSON('/products/' + product_handle + '.js', function(product) {
+      var title = product.title;
+      var type = product.type;
+      var vendor = product.vendor;
+      var price = 0;
+      var compare_price = 0;
+      var desc = product.description;
+      var images = product.images;
+      var variants = product.variants;
+      var options = product.options;
+      var url = '/products/' + product_handle;
+      $('.qv-product-title').text(title);
+      $('.qv-product-type').text(type);
+      $('.qv-product-description').html(desc);
+      $('.qv-view-product').attr('href', url);
+      $('.qv-view-type').text(type);
+      $('.qv-view-vendor').text(vendor)
+      if(quickviewAvaible == 'true'){
+        $(quickviewAddCartButton).prop('disabled', false).html(theme.strings.addToCart);
+      }
+      else{
+        $(quickviewAddCartButton).prop('disabled', true).html(theme.strings.soldOut);
+      }
+      $(product.variants).each(function(i,variants) {
+        if(variants.sku != null){
+          $('.qv-sku').addClass("show").removeClass("hide");
+          $('.qv-view-sku').text(product.variants[0].sku);
+        }
+        else{
+          $('.qv-sku').addClass("hide").removeClass("show");
+        }
+        
+      });
 
-image_embed = '<div class="collabo-handle-img"><img src="' + images[0] + '"></div>';
-image_embed = image_embed.replace('.jpg', '_800x.jpg').replace('.png', '_800x.png');
-$(quickviewThumb).append(image_embed);
+      var imageCount = $(images).length;
+      image_embed = '<div class="collabo-handle-img"><img src="' + images[0] + '"></div>';
+      image_embed = image_embed.replace('.jpg', '_800x.jpg').replace('.png', '_800x.png');
+      $(quickviewThumb).append(image_embed);
+      
+      // END
 
-// END
 
 
 
